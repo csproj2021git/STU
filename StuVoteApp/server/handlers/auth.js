f71c72765f8eeec97bc74db68487175f2e679926
Allen Bronshtein <abronshtein@aligntech.com>
2022-05-21 14:43:40 +0300

    Added polls app

---
+++ StuVoteApp/server/handlers/auth.js
@@ -0,0 +1,35 @@
+const db = require('../models')
+const jwt = require('jsonwebtoken')
+
+// Register user
+exports.register = async (req, res, next) =>{
+    try{
+        const user = await db.User.create(req.body)
+        const { id, username } = user
+        const token = jwt.sign({id,username},process.env.SECRET)
+        res.status(201).json({ id, username, token })
+    }catch(err){
+        if(err.code === 11000){
+            err.message = 'Username already taken'
+        }
+        next(err)
+    }
+}
+
+// Login user
+exports.login = async (req, res, next) =>{
+    try{
+        const user = await db.User.findOne({username: req.body.username})
+        const {id, username} = user
+        const valid = await user.comparePassword(req.body.password)
+        if(valid){
+            const token = jwt.sign({id,username},process.env.SECRET)
+            res.json({id, username, token})
+        } else{
+            throw new Error()
+        }
+    }catch(err){
+        err.message = 'Invalid username/password'
+        next(err)
+    }
+}
---
+++ StuVoteApp/server/routes/poll.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const handle = require('../handlers')
+const middlewares = require('../middlewares')
+
+//localhost:PORT/api/poll/
+router.route('/')
+.get(handle.showPolls)
+.post(middlewares.authorize, handle.createPoll)
+
+//localhost:PORT/api/poll/user
+router.route('/user')
+.get(middlewares.authorize, handle.usersPolls)
+
+//localhost:PORT/api/poll/:id
+router.route('/:id')
+.get(handle.getPoll)
+.post(middlewares.authorize, handle.vote)
+.delete(middlewares.authorize, handle.deletePoll)
+
+module.exports = router
+
