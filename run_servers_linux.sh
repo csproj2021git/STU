d83b05a161a9bb2932864745df7a7e88ecb887bf
eladoni1 <elad.shechter@live.biu.ac.il>
2022-07-26 21:41:13 +0300

    batch for running everything

--- run_servers_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd awareness
-python cv_close_eye_detect.py &
-cd ../classrooms
-node server.js &
-
--- run_servers_windows.bat
+++ run_servers_windows.bat
@@ -1,9 +1,22 @@
-::  First install python requirements :
+::  1) install python requirements :
 cd awareness
 pip install -r requirements.txt
 start python cv_close_eye_detect.py
-::  Second install javascript (classroom) requirements :
+::  2) install classroom requirements & run classroom's server [port 3030] :
 cd ..\classrooms
 start npm run dev
-:: start node server.js
-::  To be added - queries..
+:: 3) install Login requirements & run login's server and client [port 5000 & 5001 respectively]
+cd ..\login\server
+start npm run dev
+cd ..\client_
+start npm run dev
+:: 4) install Moodle requirements & run Moodle's server and client [port 7000 & 7001 respectively]
+cd ..\..\Moodle\server
+start npm run dev
+cd ..\client_
+start npm run dev
+:: 5) install Polls requirements & run Poll's server and client [port 4000 & 4001 respectively]
+cd ..\..\Polls\server
+start npm run dev
+cd ..\client_
+start npm run dev
